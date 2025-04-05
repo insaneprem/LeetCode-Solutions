116e841ea2ffe4d564a1dbbfa9f5a36d1dd327ef
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-04-05 19:26:34 +0530

    Time: 3 ms (39.68%), Space: 9.1 MB (90%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        int ans=0,temp;
+        int ans = 0, temp;
 
-        for(int i=0;i<(1<<nums.size());i++){
-            temp=0;
-            for(int j=0;j<nums.size();j++) if((i>>j) & 1) temp^=nums[j];
-            ans+=temp;
+        for (int i = 0; i < (1 << nums.size()); i++) {
+            temp = 0;
+            for (int j = 0; j < nums.size(); j++)
+                if ((i >> j) & 1)
+                    temp ^= nums[j];
+            ans += temp;
         }
 
         return ans;
