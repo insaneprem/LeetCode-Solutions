c20ef91496e242e551a588f487931c7546a77c2a
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-04-17 08:39:43 +0530

    Time: 9 ms (3.85%), Space: 16.1 MB (7.3%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-        int n = nums.size(), ans = 0;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                ans += (nums[i] == nums[j] && (i * j) % k == 0);
+        vector<int> divs;
+        unordered_map<int, int> divIndex;
+        for (int d = 1; d * d <= k; ++d) {
+            if (k % d == 0) {
+                divs.push_back(d);
+                if (d * d != k) divs.push_back(k / d);
             }
+        }
+        sort(divs.begin(), divs.end());
+        for (int i = 0; i < divs.size(); i++) divIndex[divs[i]] = i;
+
+        map<int, vector<long long>> freq;
+        long long ans = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            int g = gcd(j, k), need = k / g;
+            auto& f = freq[nums[j]];
+            if (f.empty()) f.resize(divs.size());
+
+            ans += f[divIndex[need]];
+            for (int i = 0; i < divs.size(); i++) if (j % divs[i] == 0) f[i]++;
         }
 
         return ans;
