3c6b1c79025a1159b9c3a6d5afbc5e155a5c7368
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-04-25 12:23:32 +0530

    Time: 115 ms (6.01%), Space: 124 MB (36.07%) - LeetHub

---
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        int n = nums.size();
+        map<int, int> cnt;
+        long long res = 0, prefix = 0;
+        cnt[0] = 1;
+        for (int i = 0; i < n; i++) {
+            prefix += (nums[i] % modulo == k);
+            res += cnt[(prefix - k + modulo) % modulo];
+            cnt[prefix % modulo]++;
+        }
+        return res;
+    }
+};
