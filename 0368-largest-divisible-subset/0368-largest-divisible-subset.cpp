e23765021f17d3e612b08125ec1859aa5e4e1ddb
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-05-27 14:15:31 +0530

    Time: 75 ms (5.07%), Space: 12.7 MB (20.11%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n = nums.size();
-       
-
-        ranges::sort(nums);
-        vector<pair<int, int>> dp(n, {1, -1}); 
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[j].first + 1 > dp[i].first) {
-                    dp[i].first = dp[j].first + 1;
-                    dp[i].second = j;
+        int n=nums.size();
+        vector<pair<int,int>> dp(n);
+        sort(begin(nums),end(nums));
+        
+        for(int i=0;i<n;i++) dp[i]={1,i};
+        
+        int maxi=1,maxind=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(dp[j].first>=dp[i].first && nums[i]%nums[j] == 0){
+                    dp[i].first=dp[j].first+1;
+                    dp[i].second=j;
                 }
             }
-        }
-
-        int maxi = 0;
-        for (int i = 1; i < n; i++) {
-            if (dp[i].first > dp[maxi].first) {
-                maxi = i;
+            if(dp[i].first>maxi){
+                maxi=dp[i].first;
+                maxind=i;
             }
-        }
+        } 
 
         vector<int> ans;
-        while (maxi != -1) {
-            ans.push_back(nums[maxi]);
-            maxi = dp[maxi].second;
+        for(auto i:dp) cout<<i.first<<" "<<i.second<<"\n";
+        cout<<maxind;
+        while(dp[maxind].second != maxind){
+            // cout<<maxind<<" ";
+            ans.push_back(nums[maxind]);
+            maxind=dp[maxind].second;
         }
+        ans.push_back(nums[maxind]);
 
-        reverse(ans.begin(), ans.end());
+        reverse(begin(ans),end(ans));
+
         return ans;
+
     }
 };
