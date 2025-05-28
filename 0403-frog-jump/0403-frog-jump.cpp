9eeadad40610664e4f418353e2c011ffa50711b5
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-05-28 19:46:46 +0530

    Time: 182 ms (21.55%), Space: 57 MB (35.91%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    map<int, int> mpp;
-    int n;
-    int dp[2001][2001];
-    bool solve(int curr_stone_idx, int prevjump, vector<int>& stones) {
-        if (curr_stone_idx == n - 1)
-            return 1;
-        
-        if(dp[curr_stone_idx][prevjump] != -1) return dp[curr_stone_idx][prevjump];
+    bool canCross(vector<int>& stones) {
+        int n(stones.size());
+        map<int, set<int>> mpp;
+        mpp[1].insert({1});
 
-
-        bool result = false;
-        for (int curr_jump = prevjump - 1; curr_jump <= prevjump + 1;
-             curr_jump++) {
-            if (curr_jump > 0) {
-                int next_stone = stones[curr_stone_idx] + curr_jump;
-
-                if (mpp.find(next_stone) != mpp.end())
-                    result =
-                        result || solve(mpp[next_stone], curr_jump, stones);
+        for (int i = 1; i < n; i++) {
+            for (auto prev_jump : mpp[stones[i]]) {
+                mpp[stones[i] + prev_jump - 1].insert(prev_jump - 1);
+                mpp[stones[i] + prev_jump].insert(prev_jump);
+                mpp[stones[i] + prev_jump + 1].insert(prev_jump + 1);
             }
         }
 
-        return dp[curr_stone_idx][prevjump]=result;
-    }
-    bool canCross(vector<int>& stones) {
-        n = stones.size();
-        for (int i = 0; i < n; i++)
-            mpp[stones[i]] = i;
-        
-        memset(dp,-1,sizeof(dp));
-        return solve(0, 0, stones);
+        return mpp[stones.back()].size();
     }
 };
