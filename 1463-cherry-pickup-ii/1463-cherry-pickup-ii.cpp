52ef9eac96498280c67b19b8658d37d07fcf50af
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-05-29 12:57:21 +0530

    Time: 15 ms (98.98%), Space: 14 MB (85.29%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int dp[71][71][71];
-    int n, m;
-    int solve(int row, int col1, int col2, vector<vector<int>>& grid) {
-        if (row == n)
-            return 0;
+    int dp[77][77][77];
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        memset(dp, 0, sizeof(-1));
 
-        if (dp[row][col1][col2] != -1)
-            return dp[row][col1][col2];
+        for (int row = n - 1; row >= 0; row--) {
+            for (int c1 = 0; c1 < m; c1++) {
+                for (int c2 = 0; c2 < m; c2++) {
+                    dp[row][c1][c2] = (c1 == c2)
+                                          ? grid[row][c1]
+                                          : grid[row][c1] + grid[row][c2];
 
-        int cherry = (col1 == col2) ? grid[row][col1]
-                                    : grid[row][col1] + grid[row][col2];
+                    int ans = 0;
+                    for (int i = -1; i <= 1; i++) {
+                        for (int j = -1; j <= 1; j++) {
+                            int nrow = row + 1;
+                            int nc1 = c1 + i;
+                            int nc2 = c2 + j;
 
-        int ans = 0;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                int nrow = row + 1;
-                int nc1 = col1 + i;
-                int nc2 = col2 + j;
+                            if (nc1 >= 0 && nc2 >= 0 && nc1 < m && nc2 < m)
+                                ans = max(ans, dp[nrow][nc1][nc2]);
+                        }
+                    }
 
-                if (nc1 >= 0 && nc1 < m && nc2 >= 0 && nc2 < m) {
-                    ans = max(ans, solve(nrow, nc1, nc2, grid));
+                    dp[row][c1][c2] += ans;
                 }
             }
         }
 
-        return dp[row][col1][col2] = cherry + ans;
-    }
-    int cherryPickup(vector<vector<int>>& grid) {
-        n = grid.size(), m = grid[0].size();
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, m - 1, grid);
+        return dp[0][0][m - 1];
     }
 };
