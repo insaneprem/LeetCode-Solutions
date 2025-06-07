cc373b4bd2a4b2c61d7f5887c282e1439188b83a
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-06-07 21:50:48 +0530

    Time: 13 ms (95.72%), Space: 13.8 MB (76.99%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    int n;
-    vector<int> cuts;
-    int dp[107][107];
-    int solve(int l, int r) {
-        if (r - l < 2)
-            return 0;
-
-        if (dp[l][r] != -1)
-            return dp[l][r];
-        int result = INT_MAX;
-        for (int idx = l + 1; idx <= r - 1; idx++) {
-            result = min(result,
-                         (cuts[r] - cuts[l]) + solve(l, idx) + solve(idx, r));
-        }
-
-        return dp[l][r] = result;
-    }
     int minCost(int n, vector<int>& cuts) {
-        this->n = n;
         cuts.push_back(0);
         cuts.push_back(n);
-        sort(begin(cuts), end(cuts));
-        this->cuts = cuts;
-        memset(dp, -1, sizeof(dp));
-        cout<<cuts.size();
-        return solve(0, cuts.size() - 1);
+        sort(cuts.begin(), cuts.end());
+
+        int m = cuts.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+
+        for (int len = 2; len < m; len++) {
+            for (int l = 0; l + len < m; l++) {
+                int r = l + len;
+                dp[l][r] = INT_MAX;
+                for (int i = l + 1; i < r; i++) {
+                    dp[l][r] =
+                        min(dp[l][r], dp[l][i] + dp[i][r] + cuts[r] - cuts[l]);
+                }
+            }
+        }
+
+        return dp[0][m - 1]; 
     }
 };
