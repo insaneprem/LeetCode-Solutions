e61aa46874cac2b510ae707f60b27aae3f4a60d4
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-06-09 19:29:51 +0530

    Time: 278 ms (27.18%), Space: 157.2 MB (30.04%) - LeetHub

---
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<long long>> dp(n, vector<long long>(2, 0));
+        dp[0][0] = 0;
+        dp[0][1] =  nums[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max({dp[i - 1][0], dp[i - 1][1] - nums[i]});
+            dp[i][1] = max({dp[i - 1][1], dp[i - 1][0] + nums[i]});
+        }
+
+        return max(dp[n - 1][0], dp[n - 1][1]);
+    }
+};
