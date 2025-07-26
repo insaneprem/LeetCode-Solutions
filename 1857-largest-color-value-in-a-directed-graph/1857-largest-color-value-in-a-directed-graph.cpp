7e40035a6e088e700bd12df31c9a969dc804c064
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-07-26 20:01:04 +0530

    Time: 400 ms (25.85%), Space: 175.1 MB (56.2%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,40 +1,72 @@
 class Solution {
 public:
+    bool isCyclePresent(int node, vector<vector<int>>& adj,
+                        vector<int>& visited) {
+        visited[node] = 1;
+
+        for (auto e : adj[node]) {
+            if (visited[e] == 1)
+                return true;
+            else if (visited[e] == 0) {
+                if (isCyclePresent(e, adj, visited))
+                    return true;
+            }
+        }
+
+        visited[node] = 2;
+        return false;
+    }
+
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited,
+             vector<vector<int>>& val, string& color) {
+        visited[node] = 1;
+
+        for (auto e : adj[node]) {
+            if (visited[e] == 0) {
+                dfs(e, adj, visited, val, color);
+            }
+            for (int i = 0; i < 26; i++) {
+                val[node][i] = max(val[e][i], val[node][i]);
+            }
+        }
+
+        val[node][color[node] - 'a']++;
+        visited[node] = 2;
+    }
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         int n = colors.size();
-        vector<int> indegrees(n, 0);
-        vector<vector<int>> graph(n, vector<int>());
-        for (vector<int>& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            indegrees[edge[1]]++;
+        vector<vector<int>> adj(n);
+
+        for (auto e : edges) {
+            int u = e[0], v = e[1];
+
+            adj[u].push_back(v);
         }
-        queue<int> zero_indegree;
+
+        vector<int> visited(n, 0);
         for (int i = 0; i < n; i++) {
-            if (indegrees[i] == 0) {
-                zero_indegree.push(i);
+            if (!visited[i]) {
+                if (isCyclePresent(i, adj, visited))
+                    return -1;
             }
         }
-        vector<vector<int>> counts(n, vector<int>(26, 0));
+
+        vector<vector<int>> val(n, vector<int>(26, 0));
+        visited.assign(n, 0);
+
         for (int i = 0; i < n; i++) {
-            counts[i][colors[i] - 'a']++;
-        }
-        int max_count = 0;
-        int visited = 0;
-        while (!zero_indegree.empty()) {
-            int u = zero_indegree.front();
-            zero_indegree.pop();
-            visited++;
-            for (int v : graph[u]) {
-                for (int i = 0; i < 26; i++) {
-                    counts[v][i] = max(counts[v][i], counts[u][i] + (colors[v] - 'a' == i ? 1 : 0));
-                }
-                indegrees[v]--;
-                if (indegrees[v] == 0) {
-                    zero_indegree.push(v);
-                }
+            if (!visited[i]) {
+                dfs(i, adj, visited, val, colors);
             }
-            max_count = max(max_count, *max_element(counts[u].begin(), counts[u].end()));
         }
-        return visited == n ? max_count : -1;
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 26; j++)
+                ans = max(ans, val[i][j]);
+        }
+
+        return ans;
     }
 };
