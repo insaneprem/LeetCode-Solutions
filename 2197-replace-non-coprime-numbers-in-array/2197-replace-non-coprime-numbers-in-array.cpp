fae48308866cb12f003a4651eb473aa70229f32b
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-09-16 10:59:11 +0530

    Time: 32 ms (26.85%), Space: 127.9 MB (36.11%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    vector<int> replaceNonCoprimes(vector<int>& nums) {
-        vector<int> ans(1,nums[0]);
+    vector<int> getans(stack<int>& st) {
+        vector<int> ans;
 
-        for(int i=1;i<nums.size();i++){
-            int curr=nums[i];
-
-            while(ans.size() && gcd(curr,ans.back()) >1){
-                int top=ans.back();
-                ans.pop_back();
-                curr=lcm(top,curr);
-            }
-            ans.push_back(curr);
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
         }
 
+        reverse(ans.begin(), ans.end());
+
         return ans;
+    }
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        stack<int> st;
+
+        for(auto v:nums){
+            int num = v;
+
+            while(!st.empty() && gcd(st.top(),num)>1){
+                int lc = lcm(st.top(),num);
+                st.pop();
+                num = lc;
+            }
+
+            st.push(num);
+        }
+
+        return getans(st);
     }
 };
