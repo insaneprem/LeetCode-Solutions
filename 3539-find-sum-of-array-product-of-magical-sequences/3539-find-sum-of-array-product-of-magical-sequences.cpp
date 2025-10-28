cb61a9ca6f009bb6419e4a4a0c9223e2c7b1a417
PREM KUMAR <116575323+insaneprem@users.noreply.github.com>
2025-10-28 18:20:31 +0530

    Time: 2709 ms (5.02%), Space: 68.9 MB (35.46%) - LeetHub

--- 3539-find-sum-of-array-product-of-magical-sequences/3539-find-sum-of-array-product-of-magical-sequences.cpp
+++ 3539-find-sum-of-array-product-of-magical-sequences/3539-find-sum-of-array-product-of-magical-sequences.cpp
@@ -1,61 +1,75 @@
 class Solution {
 public:
-    const int mod = 1e9 + 7;
-
-    int add(int a, int b) { return (a + b) % mod; }
-    int mul(int a, int b) { return 1LL * a * b % mod; }
-
-    int modpow(int a, int b) {
-        int res = 1;
-        while (b) {
-            if (b & 1) res = mul(res, a);
-            a = mul(a, a);
-            b >>= 1;
+    const int MOD = 1e9 + 7;
+    const int MAXN = 31;
+    int n;
+    vector<int> fact, factinv;
+    map<string,int> dp;
+    int modpow(int base, int exp) {
+        base %= MOD;
+        long long ans = 1;
+        while (exp) {
+            if (exp & 1)
+                ans = (ans * base) % MOD;
+            base = (1LL * base * base) % MOD;
+            exp >>= 1;
         }
-        return res;
+        return ans;
+    }
+
+    int modinv(int val) { return modpow(val, MOD - 2); }
+
+    void precomp() {
+        fact.resize(MAXN);
+        factinv.resize(MAXN);
+        fact[0] = 1;
+        for (int i = 1; i <= 30; i++)
+            fact[i] = (1LL * fact[i - 1] * i) % MOD;
+        factinv[30] = modinv(fact[30]);
+        for (int i = 29; i >= 0; i--)
+            factinv[i] = (1LL * factinv[i + 1] * (i + 1)) % MOD;
+    }
+
+    int ncr(int n, int r) {
+        if (n < 0 || r < 0 || r > n)
+            return 0;
+        int ans = fact[n];
+        ans = (1LL * ans * factinv[n - r]) % MOD;
+        ans = (1LL * ans * factinv[r]) % MOD;
+        return ans;
+    }
+
+    int solve(int idx, int bitsum, int m, int k, vector<int>& nums) {
+        if (m == 0 && __builtin_popcount(bitsum) == k)
+            return 1;
+
+        if (m == 0 || idx >= n)
+            return 0;
+        
+        auto key = to_string(idx) + '_' + to_string(bitsum) + '_' + to_string(m) + '_' + to_string(k);
+
+        if(dp.count(key)) return  dp[key];
+
+        int tsum = solve(idx + 1, (bitsum >> 1), m, k - (bitsum & 1), nums);
+
+        for (int freq = 1; freq <= m; freq++) {
+            int newsum = bitsum + freq;
+            long long prod = modpow(nums[idx], freq) % MOD;
+            long long ways = ncr(m, freq);
+
+            prod = (prod * ways) % MOD;
+
+            prod = (prod * solve(idx + 1, newsum >> 1, m - freq, k - (newsum & 1), nums)) % MOD;
+            tsum = (tsum + prod) % MOD;
+        }
+
+        return dp[key] = tsum % MOD;
     }
 
     int magicalSum(int m, int k, vector<int>& nums) {
-        int n = nums.size();
-        vector<int> fact(m + 1, 1), invf(m + 1, 1);
-        for (int i = 1; i <= m; i++) fact[i] = mul(fact[i - 1], i);
-        invf[m] = modpow(fact[m], mod - 2);
-        for (int i = m - 1; i >= 0; i--) invf[i] = mul(invf[i + 1], i + 1);
-
-        vector<vector<int>> pow_nums(n, vector<int>(m + 1, 1));
-        for (int i = 0; i < n; i++)
-            for (int j = 1; j <= m; j++)
-                pow_nums[i][j] = mul(pow_nums[i][j - 1], nums[i]);
-
-        int h = n + 6;
-        vector dp(2, vector(m + 1, vector(k + 1, vector<int>(m + 1))));
-        dp[0][0][0][0] = 1;
-
-        for (int i = 0; i < h; i++) {
-            dp[1] = vector(m + 1, vector(k + 1, vector<int>(m + 1, 0)));
-            for (int used = 0; used <= m; used++) {
-                for (int pop = 0; pop <= k; pop++) {
-                    for (int carry = 0; carry <= m; carry++) {
-                        int val = dp[0][used][pop][carry];
-                        if (!val) continue;
-                        if (i < n) {
-                            for (int cnt = 0; cnt + used <= m; cnt++) {
-                                int sum = carry + cnt;
-                                int bit = sum % 2, next_carry = sum / 2;
-                                if (pop + bit > k) continue;
-                                int w = mul(val, mul(pow_nums[i][cnt], invf[cnt]));
-                                dp[1][used + cnt][pop + bit][next_carry] = add(dp[1][used + cnt][pop + bit][next_carry], w);
-                            }
-                        } else {
-                            int bit = carry % 2, next_carry = carry / 2;
-                            if (pop + bit <= k)
-                                dp[1][used][pop + bit][next_carry] = add(dp[1][used][pop + bit][next_carry], val);
-                        }
-                    }
-                }
-            }
-            swap(dp[0], dp[1]);
-        }
-        return mul(dp[0][m][k][0], fact[m]);
+        this->n = nums.size();
+        dp.clear();
+        precomp();
+        return solve(0, 0, m, k, nums);
     }
 };
